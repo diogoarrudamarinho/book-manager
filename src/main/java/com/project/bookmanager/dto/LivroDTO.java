e98d6bfe69ae875ceef4faea3a35aedf4fe525ae
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-02-05 20:59:41 -0300

    docs: update LivroDTO

--- src/main/java/com/project/bookmanager/dto/LivroDTO.java
+++ src/main/java/com/project/bookmanager/dto/LivroDTO.java
@@ -1,5 +1,7 @@
 package com.project.bookmanager.dto;
 
+import java.util.List;
+
 import com.project.bookmanager.entities.Livro;
 
 public class LivroDTO {
@@ -7,20 +9,27 @@ public class LivroDTO {
     private Long id;
     private String titulo;
     private String autor;
-    private String genero;
     private Boolean lido;
     private Boolean emprestado;
+    private String imageURL;
+    private List<GeneroDTO> generos;
 
     public LivroDTO() {
     }
-
-    public LivroDTO(Livro entity){
-        this.id = entity.getId();
-        this.titulo = entity.getTitulo();
-        this.autor = entity.getAutor();
-        this.genero = entity.getGenero();
-        this.lido = entity.isLido();
-        this.emprestado = entity.isEmprestado();
+    
+    public LivroDTO(Livro livro) {
+        this.id = livro.getId();
+        this.titulo = livro.getTitulo();
+        this.lido = livro.isLido();
+        this.emprestado = livro.isEmprestado();
+        this.autor = livro.getAutor() == null ? 
+                            null : livro.getAutor();
+        this.imageURL = livro.getImageURL() == null ? 
+                            null : livro.getImageURL();
+        this.generos = livro.getGeneros()
+                            .stream()
+                            .map(GeneroDTO::new)
+                            .toList();
     }
 
     public Long getId() {
@@ -35,16 +44,19 @@ public class LivroDTO {
         return autor;
     }
 
-    public String getGenero() {
-        return genero;
+    public String getImageURL() {
+        return imageURL;
     }
 
-    public Boolean getLido() {
+    public List<GeneroDTO> getGeneros() {
+        return generos;
+    }
+
+    public Boolean isLido() {
         return lido;
     }
 
-    public Boolean getEmprestado() {
+    public Boolean isEmprestado() {
         return emprestado;
     }
-
 }
