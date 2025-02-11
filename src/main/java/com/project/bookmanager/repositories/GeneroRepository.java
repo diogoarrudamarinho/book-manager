23a8cc017605386e3621a81b68a2d0867f898930
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-02-11 00:35:50 -0300

    docs: update repositories

--- src/main/java/com/project/bookmanager/repositories/GeneroRepository.java
+++ src/main/java/com/project/bookmanager/repositories/GeneroRepository.java
@@ -3,11 +3,15 @@ package com.project.bookmanager.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project.bookmanager.entities.Genero;
 
 public interface GeneroRepository extends JpaRepository<Genero, Long> {
 
-    List<Genero> findByNomeInIgnoreCase(List<String> generos);
+    @Query( "SELECT g FROM Genero g "+ 
+            "WHERE LOWER(g.nome) IN :generos")
+    List<Genero> findAllByNomes(@Param("generos") List<String> generos);
     
 }
--- src/main/java/com/project/bookmanager/repositories/LivroRepository.java
+++ src/main/java/com/project/bookmanager/repositories/LivroRepository.java
@@ -7,32 +7,33 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.project.bookmanager.entities.Genero;
 import com.project.bookmanager.entities.Livro;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
-    @Query( "SELECT DISTINCT l FROM Livro l" +
-            "WHERE LOWER(l.nome) LIKE %:term% " +
-            "OR LOWER(l.autor) LIKE %:term% ")
+    @Query("SELECT DISTINCT l FROM Livro l " +
+           "WHERE LOWER(l.titulo) LIKE LOWER(CONCAT('%', :term, '%')) " +
+           "OR LOWER(l.autor) LIKE LOWER(CONCAT('%', :term, '%'))")
     List<Livro> findAllByTerm(@Param("term")String term);
     
-    List<Livro> findByGeneroInIgnoreCase(List<Genero> generos);
+    @Query(  "SELECT DISTINCT l FROM Livro l " +
+             "JOIN l.generos g WHERE LOWER(g.nome) IN :generos")
+    List<Livro> findAllByGeneros(@Param("generos") List<String> generos);
 
-    @Query( "SELECT l FROM Livro l" + 
-            "WHERE l.read = true")
+    @Query( "SELECT l FROM Livro l " + 
+            "WHERE l.lido = true")
     List<Livro> findAllRead();
 
-    @Query( "SELECT l FROM Livro l" + 
-            "WHERE l.read = false")
+    @Query( "SELECT l FROM Livro l " + 
+            "WHERE l.lido = false")
     List<Livro> findAllNotRead();
 
-    @Query( "SELECT l FROM Livro l" + 
+    @Query( "SELECT l FROM Livro l " + 
             "WHERE l.emprestado = true")
     List<Livro> findAllEmprestados();
 
-    @Query( "SELECT l FROM Livro l" + 
+    @Query( "SELECT l FROM Livro l " + 
             "WHERE l.emprestado = false")
     List<Livro> findAllNotEmprestados();
 }
